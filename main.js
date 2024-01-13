cd6df8c501d7efc6823999be6ce46c316ccae407
hihello-bye <137717369+hihello-bye@users.noreply.github.com>
2024-01-13 16:16:35 +0000

    create function operate
    takes an operator and two numbers and determines which operation to perform

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
 function add(a,b) {
     return a + b;
 }
+
 function subtract(a,b) {
     return a - b;
 }
+
 function divide(a,b) {
     if ( b !== 0 ) {
         return a / b;
@@ -12,9 +14,27 @@ function divide(a,b) {
         return "Not possible to divide by 0 :(";
     }
  }
+
 function multiply(a,b) {
     return a * b;
 }
+
 let number1 = 3;
 let operator = '+';
 let number2 = 6;
+
+function operate(operator,a,b) {
+    switch (operator) {
+        case '+':
+         return add(a,b);
+        case '-':
+         return subtract(a,b);
+        case '/':
+         return divide(a,b);
+        case '*':
+         return multiply(a,b);
+        default:
+         return "ERROR";
+
+    }
+}
